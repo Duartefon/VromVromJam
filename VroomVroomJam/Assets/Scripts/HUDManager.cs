23f391c6504bf0bd8f5a107e0340903503487566
Gabriel <gabrielrei2112@gmail.com>
2026-03-19 20:40:38 +0000

    starting hud controller

---
+++ VroomVroomJam/Assets/Scripts/HUDManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+public class HUDManager : MonoBehaviour
+{
+    public TMP_Text titleText, payText;
+    public MissionManager missionManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        titleText.text = missionManager.mission.missionName;
+        payText.text = missionManager.mission.TotalReward;
+    }
+}
