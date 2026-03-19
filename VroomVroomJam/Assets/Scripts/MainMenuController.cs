465ec0a4bd8d6bf6144a22ff0fdb755167fcb1b7
RickyRocky <97205567+RickyRocky@users.noreply.github.com>
2026-03-19 23:27:07 +0000

    Rough Start Menu

---
+++ VroomVroomJam/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject sceneryCamera;
+    public GameObject carCamera;
+    public GameObject startText;
+    public GameObject carTarget;
+    public Transform[] sceneryMarkers;
+
+    private int currentMarkerIndex = 0;
+
+    public float cameraSwitchTime = 4.0f;
+    public float textBlinkTime = 0.5f;
+
+    public float orbitSpeed = 20f;
+    public float slideSpeed = 1f;
+
+    void Start()
+    {
+        if (sceneryMarkers.Length > 0)
+        {
+            SnapToNextMarker();
+        }
+
+        InvokeRepeating("SwitchCameras", cameraSwitchTime, cameraSwitchTime);
+        InvokeRepeating("BlinkText", textBlinkTime, textBlinkTime);
+    }
+
+    void Update()
+    {
+        carCamera.transform.RotateAround(carTarget.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+        sceneryCamera.transform.Translate(Vector3.forward * slideSpeed * Time.deltaTime);
+    }
+
+    void SwitchCameras()
+    {
+        bool isSceneryOn = sceneryCamera.activeSelf;
+
+        if (!isSceneryOn && sceneryMarkers.Length > 0)
+        {
+            SnapToNextMarker();
+        }
+
+        sceneryCamera.SetActive(!isSceneryOn);
+        carCamera.SetActive(isSceneryOn);
+    }
+
+    void SnapToNextMarker()
+    {
+        sceneryCamera.transform.position = sceneryMarkers[currentMarkerIndex].position;
+        sceneryCamera.transform.rotation = sceneryMarkers[currentMarkerIndex].rotation;
+
+        currentMarkerIndex++;
+
+        if (currentMarkerIndex >= sceneryMarkers.Length)
+        {
+            currentMarkerIndex = 0;
+        }
+    }
+
+    void BlinkText()
+    {
+        startText.SetActive(!startText.activeSelf);
+    }
+}
